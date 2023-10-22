5e98b9ec1a7c6d3a23cf362810108381792230da
Aurio Rajaa <riorajaa2018@gmail.com>
2023-10-22 07:19:44 +0700

    Finishing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -116,7 +117,24 @@ class _HomePageState extends State<HomePage> {
                         color: Colors.red,
                         textColor: Colors.white,
                       );
-                    } else {
+                    }
+
+                    // Equal button
+                    else if (index == buttons.length - 1) {
+                      return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.deepPurple,
+                        textColor: Colors.white,
+                      );
+                    }
+
+                    // Rest of the button
+                    else {
                       return MyButton(
                         buttonTapped: () {
                           setState(() {
@@ -146,5 +164,17 @@ class _HomePageState extends State<HomePage> {
     }
 
     return false;
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer = eval.toString();
   }
 }
